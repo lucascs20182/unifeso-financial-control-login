15c7abc43022cafaf43920ce74a15f24108b3a8d
Lucas Cruz <lucascs20182@gmail.com>
2020-11-16 10:24:04 -0300

    taking the information from form

--- src/pages/CreateUser/CreateUser.jsx
+++ src/pages/CreateUser/CreateUser.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
 
 import logo from "../../assets/logo.svg";
 
 import "./CreateUser.css";
 
 export default function CreateUser() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log({ name, email, password });
+
+        // estudar axios
+    }
+
     return (
         <div class="container">
             <div className="container__create__user">
                 <img class="logo" src={logo} alt="Logo" />
-                <input className="input" type="text" placeholder="Nome completo" />
-                <input className="input" type="email" placeholder="Email" />
-                <input className="input" type="password" placeholder="Senha" />
-                <button class="button button--success" type="submit">
-                    Concluir
-                </button>
+                <form onSubmit={handleSubmit}>
+                    <input className="input" type="text" placeholder="Nome completo"
+                        value={name} onChange={event => setName(event.target.value)} />
+                    <input className="input" type="email" placeholder="Email" 
+                        value={email} onChange={event => setEmail(event.target.value)}/>
+                    <input className="input" type="password" placeholder="Senha" 
+                        value={password} onChange={event => setPassword(event.target.value)}/>
+                    <button class="button button--success" type="submit">
+                        Concluir
+                    </button>
+                </form>
             </div>
         </div>
     );
